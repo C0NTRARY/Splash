47dbeca9d570d870764cd962f2c66f1b98282d77
Shaailesh <devarma2@illinois.edu>
2015-09-26 00:05:55 -0500

    removed println

--- Splash/src/main/java/org/contrary/App.java
+++ Splash/src/main/java/org/contrary/App.java
@@ -8,7 +8,6 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("HELLO");
         get("/hello", (req, res) -> "Hello World");
     }
 }
